dffa0a19b183af431ac37debb0c7a32224c9ac30
shruti sharma <shrutijalewar@gmail.com>
2014-09-10 07:43:42 -0500

    fight

--- client/index.js
+++ client/index.js
@@ -1,17 +1,65 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template', [])
-  .controller('MainController', ['$scope', '$interval', function($scope, $interval){
-    var occupations = ['Superheroes', 'Ninjas', 'Pirates', 'Vampires', 'Aliens', 'Dragons', 'Sharks with Lasers', 'Transformers', 'I am Groot'];
+  angular.module('zombie-pets', [])
+  .controller('MainController', ['$scope', function($scope){
+    $scope.title = 'Zombie Pets';
 
-    $scope.title = 'Mean Template';
-    $scope.occupation = occupations[0];
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {health:100};
+    $scope.pets = [];
+    $scope.player1 = null;
+    $scope.player2 = null;
+    if($scope.player1.health < 0){$scope.player1.isZombie = true;}
+    if($scope.player2.health < 0) {$scope.player2.isZombie = true;}
 
-    $interval(function(){
-      var rnd = Math.floor(Math.random() * occupations.length);
-      $scope.occupation = occupations[rnd];
-    }, 500);
+    $scope.fight = function(){
+      var  x = Math.round(Math.random()),
+      dam1 = 0,
+      dam2 = 0;
+
+      if($scope.player1.health < 0){
+        dam1 = Math.floor(Math.random() * 4);
+      }else{
+        dam1 = Math.round(Math.random() * $scope.player1.weapon.damage);
+      }
+      if($scope.player2.health < 0){
+        dam2 = Math.floor(Math.random() * 4);
+      }else{
+        dam2 = Math.round(Math.random() * $scope.player1.weapon.damage);
+      }
+
+      if(x === 1){
+        $scope.player2.health = $scope.player2.health - dam1;
+        $scope.player1.health = $scope.player1.health - dam2;
+      }else{
+        $scope.player1.health = $scope.player1.health - dam2;
+        $scope.player2.health = $scope.player2.health - dam1;
+      }
+    };
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('name').focus();
+    };
+    $scope.addPet =function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100};
+    };
+
+    $scope.setPlayer = function(num){
+      $scope['player' + num] = this.p;
+    };
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
   }]);
 })();
 
